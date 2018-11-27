d9c00b453d4ce6bddaba79e0f5ea8ded62594633
liyulong6 <18709202005@163.com>
2018-11-27 08:54:31 +0800

    20181127

---
+++ 1+2+3+...+n/Sum_Solution.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Solution{
+public:
+    class Sum{
+    public:
+        Sum(){
+            ++_n;
+            _sum+=_n;
+            }
+        static void Reset(){
+            _n = 0;
+            _sum = 0;
+            }
+        static int Getsum(){
+            return _sum;
+        }
+    };
+    int Sum_Solution(int n){
+        Sum::Reset();
+        Sum* p = new Sum[n];
+        delete[] p;
+        p = nullptr;
+        return Sum::Getsum();
+    }
+private:
+    static int _n;
+    static int _sum;
+};
+
+int Solution::_n= 0;
+int Solution::_sum = 0;
+
+
+int main()
+{
+    int n = 5;
+    Solution s1;
+    cout<<s1.Sum_Solution(n)<<endl;
+    return 0;
+}
